e4a33696e2f1a603226a93b4447c0d4fabb3d531
Christian Spreng <c.spreng@zoho.com>
2016-05-09 10:38:40 +0200

    DB Connection from Janine

--- importAssertion/src/importAssertion/ImportAssertionMain.java
+++ importAssertion/src/importAssertion/ImportAssertionMain.java
@@ -1,21 +1,22 @@
 package importAssertion;
 
+import java.sql.SQLException;
 
 public class ImportAssertionMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		System.out.println("Arbeitsverzeichnis ist: " + System.getProperty("user.dir"));
-		if(args.length == 0)
-		{
+		if (args.length == 0) {
 			System.out.println("Bitte Datei angeben. Programm Ende");
 			return;
 		}
-		
+
 		new ImportAssertionMain(args[0]);
 	}
-	public ImportAssertionMain(String file)
-	{
-		System.out.println("Versuche Datei "+file+" einzulesen");
+
+	public ImportAssertionMain(String file) throws ClassNotFoundException, SQLException {
+		System.out.println("Versuche Datei " + file + " einzulesen");
 		Parser p = new Parser(file);
+		CheckDB c = new CheckDB(p);
 	}
 }
